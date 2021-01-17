9e359306987ceba155566374518f8d69e3c37c30
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-01-17 20:19:35 +0700

    добавил приложения в конфиги проекта

--- book_tracker/book_tracker/settings.py
+++ book_tracker/book_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=8zpiam+*7bl@3th!v#q^xm713hs571&(487a^$#nvuy(cqa-+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'tracker'
 ]
 
 MIDDLEWARE = [
