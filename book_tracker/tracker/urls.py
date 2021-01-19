1e4e03139ec0dca761d8251f6ae190b2c733621a
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-01-20 00:12:48 +0700

    добавил урлы логина и регистрации

--- book_tracker/tracker/urls.py
+++ book_tracker/tracker/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
+    path('register/', views.UserView.as_view({'post': 'registration'})),
+    path('login/', views.UserView.as_view({'post': 'login'})),
+
 ]
