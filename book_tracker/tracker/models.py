5133d46522b298bdcb578099c66968cfce1ca368
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-01-19 15:39:25 +0700

    добавил две модели

--- book_tracker/tracker/models.py
+++ book_tracker/tracker/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(verbose_name='Email')
+    first_name = models.CharField(verbose_name='Имя', max_length=30)
+    last_name = models.CharField(verbose_name='Фамилие', max_length=30)
+    token = models.CharField(verbose_name='Фамилие', max_length=30)
+
+
+    def __str__(self):
+        return f'User: {self.name} {self.last_name}'
+
+
+    class Meta:
+        verbose_name = u'Пользователь'
+        verbose_name_plural = u'Пользователи'
+
+
+class Book(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=30)
+    author_name = models.CharField(verbose_name='Фамилие', max_length=60)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'Book: {self.name}\tAuthor: {self.author_name}'
+
+
+    class Meta:
+        verbose_name = u'Книга'
+        verbose_name_plural = u'Книги'
