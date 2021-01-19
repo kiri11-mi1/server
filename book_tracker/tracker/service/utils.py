6ac2af3e00403fe01217af753fdc7ef810018dea
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-01-20 00:22:23 +0700

    вынес создание хэша в utils

--- book_tracker/tracker/service/utils.py
+++ book_tracker/tracker/service/utils.py
@@ -1,6 +1,12 @@
 from random import choice, randint
 from string import ascii_letters
+import hashlib
+
 
 def get_token():
     ALPHABET = ascii_letters + '1234567890!@#$%^*()!№;:'
     return ''.join([choice(ALPHABET) for _ in range(randint(10, 30))])
+
+
+def make_password(password):
+    return hashlib.md5(password.encode()).hexdigest()
