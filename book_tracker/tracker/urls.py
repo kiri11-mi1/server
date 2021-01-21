a4e7b1809b6b0103cca96c67359ad9eed003a24e
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-01-21 15:01:24 +0700

    довали методы get, post, put, delete

--- book_tracker/tracker/urls.py
+++ book_tracker/tracker/urls.py
@@ -4,5 +4,12 @@ from . import views
 urlpatterns = [
     path('register/', views.UserView.as_view({'post': 'registration'})),
     path('login/', views.UserView.as_view({'post': 'login'})),
+    path('delete/', views.UserView.as_view({'delete': 'delete'})),
+    path('book/', views.BookView.as_view({
+                'post': 'add',
+                'get': 'get',
+                'put': 'update',
+                'delete': 'delete',
+    })),
 
 ]
