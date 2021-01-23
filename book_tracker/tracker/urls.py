c5af9c312450290ce8ae7b006caa1ccc797c0705
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-01-23 18:54:38 +0700

    подписал префиксы user для методов связанных с юзерами

--- book_tracker/tracker/urls.py
+++ book_tracker/tracker/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.UserView.as_view({'post': 'registration'})),
-    path('login/', views.UserView.as_view({'post': 'login'})),
-    path('delete/', views.UserView.as_view({'delete': 'delete'})),
+    path('user/register/', views.UserView.as_view({'post': 'registration'})),
+    path('user/login/', views.UserView.as_view({'post': 'login'})),
+    path('user/delete/', views.UserView.as_view({'delete': 'delete'})),
     path('book/', views.BookView.as_view({
                 'post': 'add',
                 'get': 'get',
                 'put': 'update',
                 'delete': 'delete',
     })),
-
 ]
