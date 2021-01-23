353b1d1ba90c58854209302f4c160b384dd3d546
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-01-23 18:53:40 +0700

    добавил приложение доки и поменял язык в админке на рсский

--- book_tracker/book_tracker/settings.py
+++ book_tracker/book_tracker/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tracker'
+    'tracker',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
